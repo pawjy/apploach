8d2fed5f2c27a141a8e25a3b1bf02098de13c17a
Wakaba <wakaba@suikawiki.org>
2022-06-24 01:33:24 +0900

    drop circleci flag

--- t_deps/bin/env-for-circleci.pl
+++ t_deps/bin/env-for-circleci.pl
@@ -12,7 +12,6 @@ my $ac = AbortController->new;
 ApploachSS->run (
   app_docker_image => $ENV{TEST_APP_DOCKER_IMAGE},
   mysqld_database_name_suffix => '_test',
-  docker_net_host => 1,
   no_set_uid => 1,
   write_ss_env => 1,
   signal => $ac->signal,
@@ -24,7 +23,7 @@ ApploachSS->run (
 
 =head1 LICENSE
 
-Copyright 2018-2019 Wakaba <wakaba@suikawiki.org>.
+Copyright 2018-2022 Wakaba <wakaba@suikawiki.org>.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
